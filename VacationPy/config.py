772bd7d0381014546b9dd2208cbdb5912ee72a3b
slyssy <stephen.lyssy@gmail.com>
2020-09-23 23:32:11 +0000

    Added places key.

---
+++ VacationPy/config.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "7d1f1f53ce4d06cef84d7218976f4302"
+
+# Google API Key
+g_key = "AIzaSyC96NNuOTWn_4a-Hg8WlNz5Ghv6tp7AFow"
+
+#Mapbox Token
+mapbox_token = "pk.eyJ1Ijoic2x5c3N5IiwiYSI6ImNrZjl3MGl6bDBwd2Eyd3Jxc3lkcnpoZmcifQ.e7I6-TJ6eJx2yXOvgeKZLQ"
